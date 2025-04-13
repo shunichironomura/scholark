1da80e6ec8e13af721ac379ae5f52ead9473a8b4
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-13 17:46:30 +0900

    remove proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), cloudflare(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8787',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  plugins: [react(), cloudflare(), tailwindcss()]
 });
