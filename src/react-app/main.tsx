e1e0091527089841f42e6bc405abbd65e22fb33b
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-13 17:46:47 +0900

    Wrap with GoogleOAuthProvider

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-
+import { GOOGLE_CLIENT_ID } from "../shared/auth";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
 );
