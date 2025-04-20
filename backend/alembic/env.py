01166d67e9fa36cb7832b52b0cc724500898e404
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-20 17:35:08 +0900

    update alembic config

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -26,6 +26,11 @@ target_metadata = None
 # ... etc.
 
 
+def get_url() -> str:
+    # TODO: Implement a function to get the database URL
+    raise NotImplementedError
+
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -38,7 +43,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,16 +62,16 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
