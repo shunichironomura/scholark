acedf10130b66753636084b2758646099bcd8d61
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-21 11:36:01 +0900

    Use clientLoader for data fetching

--- frontend/app/routes/conferences.tsx
+++ frontend/app/routes/conferences.tsx
@@ -1,45 +1,38 @@
 import { useState, useEffect } from "react"
 import { Card, CardHeader, CardTitle } from "~/components/ui/card"
-import type { Route } from "./+types/home"
+import type { Route } from "./+types/conferences"
 import { conferencesReadConferences } from '~/client/sdk.gen';
 import type { ConferencesPublic, ConferencePublic } from "~/client/types.gen";
 
-export default function Conferences() {
-  const [conferences, setConferences] = useState<ConferencesPublic>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+export async function clientLoader({ }: Route.ClientLoaderArgs) {
+  const { data, error } = await conferencesReadConferences();
+  if (error) {
+    throw new Response("Error fetching conferences", { status: 500 });
+  }
+  return { data };
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await conferencesReadConferences();
-      setLoading(false)
-      if (error) {
-        console.error("Error fetching conferences:", error);
-        return;
-      }
-      setConferences(data);
-    }
+export default function Conferences({
+  loaderData,
+}: Route.ComponentProps) {
 
-    fetchData()
-  }, []);
+  const { data } = loaderData;
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh p-4">
       <h1 className="text-2xl font-bold mb-4">Conferences</h1>
 
-      {conferences && (
-        <div>
-          {conferences.data.map((conference: ConferencePublic) => (
-            <div>
-              {JSON.stringify(conference, null, 2)}
-            </div>
+      {data && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.data.map((conference: ConferencePublic) => (
+            <Card className="w-[300px]">
+              <CardHeader>
+                <CardTitle>{conference.name}</CardTitle>
+              </CardHeader>
+            </Card>
           ))}
         </div>
       )}
-
-
-
-
     </div>
   );
 }
