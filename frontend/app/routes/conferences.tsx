c111a35869eaa8dc8519152eefd17d249258d8dc
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-21 11:13:05 +0900

    Show conferences as JSON

--- frontend/app/routes/conferences.tsx
+++ frontend/app/routes/conferences.tsx
@@ -1,39 +1,45 @@
 import { useState, useEffect } from "react"
-
+import { Card, CardHeader, CardTitle } from "~/components/ui/card"
 import type { Route } from "./+types/home"
 import { conferencesReadConferences } from '~/client/sdk.gen';
-import type { ConferencesPublic } from "~/client/types.gen";
+import type { ConferencesPublic, ConferencePublic } from "~/client/types.gen";
 
 export default function Conferences() {
-  const conferences: ConferencesPublic = {
-    "data": [
-      {
-        "name": "Ukaren",
-        "start_date": "2025-04-20",
-        "end_date": "2025-04-20",
-        "location": "string",
-        "website_url": "string",
-        "abstract_deadline": "2025-04-20T10:44:55.066000",
-        "paper_deadline": "2025-04-20T10:44:55.066000",
-        "id": "de85b09b-a98e-4922-ac9b-968e5af52db5"
+  const [conferences, setConferences] = useState<ConferencesPublic>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await conferencesReadConferences();
+      setLoading(false)
+      if (error) {
+        console.error("Error fetching conferences:", error);
+        return;
       }
-    ],
-    "count": 1
-  }
+      setConferences(data);
+    }
+
+    fetchData()
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh p-4">
-      <h1 className="text-2xl font-bold mb-4">API Response</h1>
-
+      <h1 className="text-2xl font-bold mb-4">Conferences</h1>
 
       {conferences && (
-        <div className="w-full max-w-4xl overflow-auto">
-          <pre className="bg-gray-100 p-4 rounded text-sm">
-            {JSON.stringify(conferences, null, 2)}
-          </pre>
+        <div>
+          {conferences.data.map((conference: ConferencePublic) => (
+            <div>
+              {JSON.stringify(conference, null, 2)}
+            </div>
+          ))}
         </div>
       )}
 
+
+
+
     </div>
-  )
+  );
 }
