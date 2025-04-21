a9b674f28e53e7d641c6e94ef46d22f0cc0877b8
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-21 11:39:42 +0900

    rename variable

--- frontend/app/routes/conferences.tsx
+++ frontend/app/routes/conferences.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react"
 import { Card, CardHeader, CardTitle } from "~/components/ui/card"
 import type { Route } from "./+types/conferences"
 import { conferencesReadConferences } from '~/client/sdk.gen';
-import type { ConferencesPublic, ConferencePublic } from "~/client/types.gen";
+import type { ConferencePublic } from "~/client/types.gen";
 
 export async function clientLoader({ }: Route.ClientLoaderArgs) {
   const { data, error } = await conferencesReadConferences();
@@ -16,15 +15,15 @@ export default function Conferences({
   loaderData,
 }: Route.ComponentProps) {
 
-  const { data } = loaderData;
+  const { data: conferences } = loaderData;
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh p-4">
       <h1 className="text-2xl font-bold mb-4">Conferences</h1>
 
-      {data && (
+      {conferences && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {data.data.map((conference: ConferencePublic) => (
+          {conferences.data.map((conference: ConferencePublic) => (
             <Card className="w-[300px]">
               <CardHeader>
                 <CardTitle>{conference.name}</CardTitle>
