80587efdbc621db291cd5d87eebedea182efffab
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-26 18:22:29 +0900

    Remove comments

--- backend/src/scholark/api/routes/login.py
+++ backend/src/scholark/api/routes/login.py
@@ -29,29 +29,3 @@ def login_access_token(
 def test_token(current_user: CurrentUser) -> Any:
     """Test access token."""
     return current_user
-
-
-# @router.post("/")
-# def login(
-#     auth_provider: AuthProviderDep,
-#     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-#     response: Response,
-# ) -> LoginResponse:
-#     """OAuth2 compatible token login, get an access token for future requests."""
-#     user = auth_provider.authenticate(
-#         username=form_data.username,
-#         password=form_data.password,
-#     )
-#     if not user:
-#         raise HTTPException(status_code=400, detail="Incorrect username or password")
-#     jwt = Token(access_token=security.create_access_token(user.id))
-#     response.set_cookie(
-#         key="access_token",
-#         value=jwt.access_token,
-#         max_age=int(timedelta(minutes=10).total_seconds()),
-#         httponly=True,
-#         secure=True,
-#         samesite="lax",
-#     )
-
-#     return LoginResponse(user_id=user.id)
