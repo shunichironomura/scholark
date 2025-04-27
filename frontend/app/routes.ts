30e1253923eb56079f0551647173d99aa684ac3e
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-04-28 05:23:21 +0900

    Change the order of routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -14,8 +14,8 @@ export default [
     route("conferences/:conferenceId/tags", "routes/conference-tags.tsx"),
     route("timeline", "routes/timeline.tsx"),
     route("settings", "routes/settings.tsx"),
+    route("tags/new", "routes/create-tag.tsx"),
     route("tags/:tagId/edit", "routes/edit-tag.tsx"),
     route("tags/:tagId/delete", "routes/delete-tag.tsx"),
-    route("tags/new", "routes/create-tag.tsx"),
   ])
 ] satisfies RouteConfig;
