a3a6e94a548a73dc6f05fdca48d2dc53c67b4208
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2026-04-02 22:44:19 +0900

    Run vp migrate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  fmt: {},
+  lint: {"options":{"typeAware":true,"typeCheck":true}},
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
